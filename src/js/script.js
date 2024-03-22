03a61ff3e0fc427f58960bc3b34359548d2a8e0c
Ainur <lihodeikvint@gmail.com>
2024-03-22 18:25:42 +0500

    add meal card style and data hydration

--- src/js/script.js
+++ src/js/script.js
@@ -33,20 +33,60 @@ const recipeCountEl = document.querySelector('.recipe-count');
 const mealCardEl = document.querySelector('.meal-card');
 const mealTitleEl = document.querySelector('.meal-title');
 const mealImageEl = document.querySelector('.meal-image');
-const mealInstractionEl = document.querySelector('.meal-instraction');
-const mealIngredientEl = document.querySelector('.meal-ingredient');
-const mealVideoEl = document.querySelector('.meal-ingredient');
+const mealIngredientEl = document.querySelector('.meal-ingredients');
+const mealIngredientTbodyEl = mealIngredientEl.getElementsByTagName('tbody')[0];
+const mealInstructionsEl = document.querySelector('.meal-instructions');
+const mealVideoEl = document.querySelector('.meal-video');
 
 function showRecipeCount(meals) {
   let recipeCountText;
-  if (!meals.meals) {
+  if (!meals) {
     recipeCountText = 'No recipes found ☹️';
-  } else if (meals.meals.length === 1) {
-    recipeCountText = `Found ${meals.meals.length} recipe`;
+  } else if (meals.length === 1) {
+    recipeCountText = `Found ${meals.length} recipe`;
   } else {
-    recipeCountText = `Found ${meals.meals.length} recipes`;
+    recipeCountText = `Found ${meals.length} recipes`;
   }
   recipeCountEl.textContent = recipeCountText;
+}
+
+function showMealCard(meals) {
+  mealCardEl.classList.remove('hidden');
+  mealTitleEl.textContent = meals[0].strMeal;
+  mealImageEl.setAttribute('src', meals[0].strMealThumb);
+  mealInstructionsEl.textContent = meals[0].strInstructions;
+  const videoParams = new URL(meals[0].strYoutube).searchParams;
+  const videoId = videoParams.get('v');
+  mealVideoEl.setAttribute('src', `https://www.youtube.com/embed/${videoId}`);
+  const ingredients = extractIngredients(meals[0]);
+  fillIngredientTable(ingredients);
+}
+
+function extractIngredients(meal) {
+  const ingredients = [];
+
+  for (let key in meal) {
+    if (key.includes('strIngredient') && meal[key]) {
+      let ingredientNum = key.match(/\d+/)[0];
+      let strIngredient = meal[key];
+      let strMeasure = meal[`strMeasure${ingredientNum}`];
+      ingredients.push({ strIngredient, strMeasure });
+    }
+  }
+
+  return ingredients;
+}
+
+function fillIngredientTable(ingredients) {
+  ingredients.forEach((ingredient, index) => {
+    const trEl = document.createElement('tr');
+    const ingredientTdEl = document.createElement('td');
+    const measureTdEl = document.createElement('td');
+    ingredientTdEl.textContent = ingredient.strIngredient;
+    measureTdEl.textContent = ingredient.strMeasure;
+    trEl.append(ingredientTdEl, measureTdEl);
+    mealIngredientTbodyEl.appendChild(trEl);
+  });
 }
 
 searchButtonEl.addEventListener('click', async event => {
@@ -60,6 +100,5 @@ searchButtonEl.addEventListener('click', async event => {
   const meals = await getMeals(searchInputEl.value);
   console.log(meals);
   showRecipeCount(meals);
-
-  const meal = meals.meals[0];
+  showMealCard(meals);
 });
