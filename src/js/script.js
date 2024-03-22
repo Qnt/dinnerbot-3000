97136d109f9a18e7181fad5418d5acf516def933
Ainur <lihodeikvint@gmail.com>
2024-03-22 16:05:49 +0500

    change open_props from node_modules to CDN import add fetching meal data by id

--- src/js/script.js
+++ src/js/script.js
@@ -7,16 +7,49 @@ async function getMeals(searchIngredient) {
     return;
     //TODO propper error handling
   }
-  const data = await res.json();
-  console.log(data);
+  const mealsShort = await res.json();
 
-  return data;
+  const mealsDetailed = await Promise.all(
+    mealsShort.meals.map(async meal => {
+      const res = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal.idMeal}`
+      );
+      if (!res.ok) {
+        console.log('oops');
+        return;
+        //TODO propper error handling
+      }
+      const mealDetailed = await res.json();
+      return mealDetailed.meals[0];
+    })
+  );
+
+  return mealsDetailed;
 }
 
 const searchInputEl = document.getElementById('search-input');
 const searchButtonEl = document.querySelector('.search-button');
+const recipeCountEl = document.querySelector('.recipe-count');
+const mealCardEl = document.querySelector('.meal-card');
+const mealTitleEl = document.querySelector('.meal-title');
+const mealImageEl = document.querySelector('.meal-image');
+const mealInstractionEl = document.querySelector('.meal-instraction');
+const mealIngredientEl = document.querySelector('.meal-ingredient');
+const mealVideoEl = document.querySelector('.meal-ingredient');
 
-searchButtonEl.addEventListener('click', event => {
+function showRecipeCount(meals) {
+  let recipeCountText;
+  if (!meals.meals) {
+    recipeCountText = 'No recipes found ☹️';
+  } else if (meals.meals.length === 1) {
+    recipeCountText = `Found ${meals.meals.length} recipe`;
+  } else {
+    recipeCountText = `Found ${meals.meals.length} recipes`;
+  }
+  recipeCountEl.textContent = recipeCountText;
+}
+
+searchButtonEl.addEventListener('click', async event => {
   event.preventDefault();
   const searchValue = searchInputEl.value;
 
@@ -24,5 +57,9 @@ searchButtonEl.addEventListener('click', event => {
     console.log('Enter valid value');
     return;
   }
-  const meals = getMeals(searchInputEl.value);
+  const meals = await getMeals(searchInputEl.value);
+  console.log(meals);
+  showRecipeCount(meals);
+
+  const meal = meals.meals[0];
 });
